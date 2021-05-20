9a5b19c5b0862d315d8b7c84b4b8062cf5760be8
win <winlwinoocu@gmail.com>
2021-05-20 13:50:53 +0630

    modify function

--- bin/server.dart
+++ bin/server.dart
@@ -5,11 +5,7 @@ class AlbumService extends AlbumServiceBase {
   @override
   Future<AlbumResponse> getAlbum(ServiceCall call, AlbumRequest request) async {
     if (request.id > 0) {
-      final album = albums
-          .where((album) => album['id'] == request.id)
-          .map((album) => Album.fromJson(
-              '{"1": ${album['id']}, "2": "${album['title']}" }'))
-          .toList();
+      final album = findAlbums(request.id);
       return AlbumResponse()..albums.addAll(album);
     }
     final albumList = albums
@@ -18,6 +14,16 @@ class AlbumService extends AlbumServiceBase {
         .toList();
     return AlbumResponse()..albums.addAll(albumList);
   }
+
+  List<Album> findAlbums(int id) {
+    return albums
+        .where((album) => album['id'] == id)
+        .map(convertToAlbum)
+        .toList();
+  }
+
+  Album convertToAlbum(Map album) =>
+      Album.fromJson('{"1": ${album['id']}, "2": "${album['title']}" }');
 }
 
 void main(List<String> args) async {
